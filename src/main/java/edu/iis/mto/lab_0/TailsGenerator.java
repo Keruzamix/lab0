ba5c5b6062276de4b899e86145d7a4b61e3e5201
Mazurek.Patryk <220981@edu.p.lodz.pl>
2020-03-03 19:05:11 +0100

    first

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,13 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList<String> tail = new ArrayList<>();
+
+        for(int i=0; i<value.length(); i++){
+            tail.add(i, value.substring(i,value.length()));
+        }
+
+        return tail;
     }
 
 }
